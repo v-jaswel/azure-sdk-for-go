a20df15e01cc4809f2e55cae703ce5465df4402f
Jason Wells <30323598+v-jaswel@users.noreply.github.com>
2020-09-12 09:31:12 -0700

    Fix JSON mappings for OperationStatus

--- services/cognitiveservices/v1.0/face/models.go
+++ services/cognitiveservices/v1.0/face/models.go
@@ -415,11 +415,11 @@ type OperationStatus struct {
 	// Status - Operation status: notstarted, running, succeeded, failed. If the operation is requested and waiting to perform, the status is notstarted. If the operation is ongoing in backend, the status is running. Status succeeded means the operation is completed successfully, specifically for snapshot taking operation, it illustrates the snapshot is well taken and ready to apply, and for snapshot applying operation, it presents the target object has finished creating by the snapshot and ready to be used. Status failed is often caused by editing the source object while taking the snapshot or editing the target object while applying the snapshot before completion, see the field "message" to check the failure reason. Possible values include: 'Notstarted', 'Running', 'Succeeded', 'Failed'
 	Status OperationStatusType `json:"status,omitempty"`
 	// CreatedTime - A combined UTC date and time string that describes the time when the operation (take or apply a snapshot) is requested. E.g. 2018-12-25T11:41:02.2331413Z.
-	CreatedTime *date.Time `json:"createdTime,omitempty"`
+	CreatedTime *date.Time `json:"createdDateTime,omitempty"`
 	// LastActionTime - A combined UTC date and time string that describes the last time the operation (take or apply a snapshot) is actively migrating data. The lastActionTime will keep increasing until the operation finishes. E.g. 2018-12-25T11:51:27.8705696Z.
 	LastActionTime *date.Time `json:"lastActionTime,omitempty"`
 	// ResourceLocation - When the operation succeeds successfully, for snapshot taking operation the snapshot id will be included in this field, and for snapshot applying operation, the path to get the target object will be returned in this field.
-	ResourceLocation *string `json:"resourceLocation,omitempty"`
+	ResourceLocation *string `json:"location,omitempty"`
 	// Message - Show failure message when operation fails (omitted when operation succeeds).
 	Message *string `json:"message,omitempty"`
 }
